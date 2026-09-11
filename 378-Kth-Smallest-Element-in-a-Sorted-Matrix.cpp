ccc5ad48eded7eee9934948b446fdbbed3a7648b
Mohammad Bharmal <mohammadbharmal36@gmail.com>
2026-09-12 00:58:20 +0530

    [378] [Time Beats: 60.88%] [Memory Beats: 11.98%] - LeetPush

---
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        vector<int> v;
+        for(auto d:matrix){
+            for(auto j:d){
+                v.push_back(j);}
+        }sort(v.begin(),v.end());
+        return v[k-1];}
+};
