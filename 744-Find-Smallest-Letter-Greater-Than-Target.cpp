aaa1ee126b87634c181d22fd949ee16d072b9fad
Mohammad Bharmal <mohammadbharmal36@gmail.com>
2026-09-13 22:33:59 +0530

    [744] [Time Beats: 100.00%] [Memory Beats: 23.90%] - LeetPush

--- 744-Find-Smallest-Letter-Greater-Than-Target.cpp
+++ 744-Find-Smallest-Letter-Greater-Than-Target.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        auto it = upper_bound(letters.begin(), letters.end(), target);
-        if(it == letters.end())
+        auto it = upper_bound(letters.begin(),letters.end(),target);
+        if(it==letters.end())
             return letters[0];
             return *it;
     }
