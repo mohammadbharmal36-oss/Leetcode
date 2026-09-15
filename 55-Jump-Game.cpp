20fd497ad2c6df333b777c15c454dc519bae97fa
Mohammad Bharmal <mohammadbharmal36@gmail.com>
2026-09-15 23:33:00 +0530

    [55] [Time Beats: 100.00%] [Memory Beats: 83.26%] - LeetPush

---
+++ 55-Jump-Game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int x=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>x){
+                return false;
+            }
+            x=max(x,i+nums[i]);
+            if(x>=nums.size()-1){
+                return true;
+            }
+        }
+        
+        return true;  
+    }
+};
