f2cf5a25b8a333265471d18ad0b3b6184370ddac
Mohammad Bharmal <mohammadbharmal36@gmail.com>
2026-09-17 23:41:00 +0530

    [819] [Time Beats: 29.32%] [Memory Beats: 65.62%] - LeetPush

---
+++ 819-Most-Common-Word.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_map<string,int> m;
+        string word;
+        for(auto &x:paragraph){
+            if(x>='A'&&x<='Z'){
+                x=x+32;
+            }
+            else if(x<'a'||x>'z'){
+                x=' ';
+            }
+        }
+        stringstream ss(paragraph);
+        while(ss>>word){
+            if(find(banned.begin(),banned.end(),word)==banned.end()){
+                m[word]++;
+            }
+        }
+        string ans;
+        int mx=0;
+        for(auto k:m){
+            if(k.second>mx){
+                mx=k.second;
+                ans=k.first;
+            }
+        }
+        return ans;
+    }
+};
